c7fb1c772c83f13cd9cd6d60d5f8315733116161
jazzbuck <enterleft@exitright.co.uk>
2023-01-15 18:19:35 +0000

    ok

--- jazzbuck_automation/retrieve_pocket.py
+++ jazzbuck_automation/retrieve_pocket.py
@@ -1,2 +1,49 @@
-# import requests as re
-# import orjson as json
+import requests as re
+import orjson as json
+from dataclasses import dataclass
+import datetime
+import webbrowser
+from typing import Tuple
+
+
+@dataclass
+class PocketSession:
+    """
+    PocketSession stores state data about interaction with Pocket.
+    """
+
+    username: str
+    password: str
+    consumer_key: str
+    token: str
+
+    def get_since(
+        self,
+        date: datetime.datetime,
+    ) -> None:
+        """
+        get_since retrieves saved items since date and saves to payload
+        """
+
+
+def authenticate_pocket(
+    consumer_key: str,
+) -> Tuple[str, str]:
+    """
+    authenticates with pocket
+    """
+    method_url = "https://getpocket.com/v3/oauth/request"
+    payload = {"consumer_key": consumer_key, "redirect": "https://dis.repair"}
+    r = re.post(method_url, data=json.dumps(payload))
+    code = r.json()["code"]
+    webbrowser.open(
+        f"https://getpocket.com/auth/authorize?request_token\
+        ={code}&redirect_uri=https://dis.repair"
+    )
+    input("Press any key to continue...")
+    payload = {"consumer_key": consumer_key, "code": code}
+    s = re.post("https://getpocket.com/v3/oauth/authorize", data=json.dumps(payload))
+    access_token = s.json()["access_token"]
+    username = s.json()["username"]
+
+    return username, access_token
