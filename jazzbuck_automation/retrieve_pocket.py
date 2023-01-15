34cfc93d7af72ca99b27ae4124287856df6ec086
jazzbuck <enterleft@exitright.co.uk>
2023-01-15 19:21:29 +0000

    pocket session

--- jazzbuck_automation/retrieve_pocket.py
+++ jazzbuck_automation/retrieve_pocket.py
@@ -2,6 +2,7 @@ import requests as re
 import json as json
 from dataclasses import dataclass
 import datetime
+import time
 import webbrowser
 from typing import Tuple
 
@@ -13,17 +14,41 @@ class PocketSession:
     """
 
     username: str
-    password: str
     consumer_key: str
-    token: str
+    access_token: str
 
     def get_since(
         self,
-        date: datetime.datetime,
+        date: datetime.datetime = datetime.datetime.now() - datetime.timedelta(hours=1),
     ) -> None:
         """
-        get_since retrieves saved items since date and saves to payload
+        get_since retrieves saved items since datetime (by default 1 hour ago) and saves to payload
         """
+        method_url = "https://getpocket.com/v3/get"
+        consumer_key = self.consumer_key
+        access_token = self.access_token
+
+        headers = {"Content-Type": "application/json"}
+        payload = {
+            "consumer_key": consumer_key,
+            "access_token": access_token,
+            "since": time.mktime(date.timetuple()),
+            "detailType": "complete",
+        }
+
+        r = re.post(method_url, data=json.dumps(payload), headers=headers)
+
+        self.data = r.json()["list"]
+
+    def write_data(
+        self,
+        filename: str,
+    ) -> None:
+        """
+        write data in class to filename
+        """
+        with open(filename, "w") as fp:
+            json.dump(self.data, fp)
 
 
 def authenticate_pocket(
